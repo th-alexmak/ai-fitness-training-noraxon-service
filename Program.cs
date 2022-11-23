851ca42b12218fdf64b2462e92c0541b8851dab3
T.H. Alex MAK <alexmak@limepage.com.hk>
2022-11-23 16:56:18 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using Noraxon_Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
