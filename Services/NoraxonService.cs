fa461d7e4a82d798eb4ff734cd87f73ad97c9de8
T.H. Alex MAK <alexmak@limepage.com.hk>
2022-11-24 17:44:14 +0800

    Implement get emg sensors

--- Services/NoraxonService.cs
+++ Services/NoraxonService.cs
@@ -6,6 +6,7 @@ namespace NoraxonService.Services
 {
     public class NoraxonService : Noraxon.NoraxonBase
     {
+        private readonly static object locker = new object();
         DeviceManager deviceManager;
 
         public NoraxonService()
@@ -18,6 +19,32 @@ namespace NoraxonService.Services
         {
             deviceManager.Setup(0);
             return request;
+        }
+
+        public override async Task<GetEmgSensorsResponse> GetEmgSensors(Empty request, ServerCallContext context)
+        {
+            var result = new GetEmgSensorsResponse();
+            lock (locker)
+            {
+                var device = deviceManager.GetCurrentDevice();
+                device.SetComponentFilterTags("type.input.analog.emg");
+
+                for (int i = 0; i < device.GetComponentCount(); i++)
+                {
+                    var component = device.GetComponent(i);
+                    var tags = component.GetTags();
+
+                    foreach (string tag in tags)
+                    {
+                        if (!tag.StartsWith("line.")) continue;
+
+                        result.EmgSensors.Add(new EmgSensor() { SerialNumber = tag.Replace("line.", "") });
+                        break;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
