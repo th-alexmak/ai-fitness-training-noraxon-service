5491ff0787850b4a339cdba19089f1883e45ccaf
T.H. Alex MAK <alexmak@limepage.com.hk>
2022-11-24 16:48:18 +0800

    Setup windows service

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-using Noraxon_Service;
+using Microsoft.Extensions.Hosting.WindowsServices;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+using NoraxonService.Services;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService()
+    ? AppContext.BaseDirectory
+    : default,
+});
 
-host.Run();
+builder.Host.UseWindowsService();
+
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<NoraxonService.Services.NoraxonService>();
+app.MapGrpcReflectionService();
+
+app.Run();
---
+++ Services/NoraxonService.cs
@@ -0,0 +1,23 @@
+﻿using Easy2AcquireCom;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace NoraxonService.Services
+{
+    public class NoraxonService : Noraxon.NoraxonBase
+    {
+        DeviceManager deviceManager;
+
+        public NoraxonService()
+        {
+            deviceManager = new DeviceManager();
+            deviceManager.Initialize("");
+        }
+
+        public override async Task<Empty> Setup(Empty request, ServerCallContext context)
+        {
+            deviceManager.Setup(0);
+            return request;
+        }
+    }
+}
